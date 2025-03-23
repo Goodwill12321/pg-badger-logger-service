333627f0e34b69ef974b5a2a4944b8070c4c72ce
Viktor Kostenich <kostenich.vv@gmail.com>
2025-03-23 22:55:01 +0300

    change algorithm of exchange to direct exchange without preprocessing. kill all processes, that started with report

--- src/handlers/report.go
+++ src/handlers/report.go
@@ -1,16 +1,16 @@
 package handlers
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
 	"sync"
+	"syscall"
 	"time"
 
 	"pg-badger-service/src/config"
@@ -19,21 +19,21 @@ import (
 )
 
 type ReportProcess struct {
-	Cmd       *exec.Cmd
+	Cmd       []*exec.Cmd
 	StartTime time.Time
 }
 
 var reportProcesses sync.Map
 
-func getOSCommand(command string) *exec.Cmd {
+/*func getOSCommand(command string) *exec.Cmd {
 	if runtime.GOOS == "windows" {
-		cmd := exec.Command("cmd", "/C", command)
+		cmd := exec.Command("cmd", "/C", "\""+command+"\"")
 		return cmd
 	} else {
 		cmd := exec.Command("/bin/sh", "-c", command)
 		return cmd
 	}
-}
+}*/
 
 func GenerateReport(c *gin.Context, serverName string, reportDir string) {
 	logFile := c.PostForm("logFile")
@@ -59,6 +59,7 @@ func GenerateReport(c *gin.Context, serverName string, reportDir string) {
 	// Generate report filename
 	reportName := strings.TrimSuffix(logFile, ".log") + ".html"
 	reportPath := filepath.Join(serverReportDir, reportName)
+	//reportKey := filepath.Join(serverReportDir, logFile)
 	outputPath := filepath.Join(serverReportDir, strings.TrimSuffix(logFile, ".log")+".out")
 
 	// Check if report is already being generated
@@ -67,27 +68,6 @@ func GenerateReport(c *gin.Context, serverName string, reportDir string) {
 		return
 	}
 
-	// Create psql command to fetch log content
-	/*psqlCmd := exec.Command("psql", "-A", "-q",
-	"-h", server.Host,
-	"-p", fmt.Sprintf("%d", server.Port),
-	"-U", server.User,
-	"-d", server.Database,
-	"-c", fmt.Sprintf("\"SELECT pg_read_file('pg_log/%s', 0, 1000000000000);\"", logFile))*/
-
-	psqlCmd := getOSCommand(fmt.Sprintf("psql -A -q -h %s -p %d -U %s -d %s -c \"SELECT pg_read_file('pg_log/%s');\"",
-		server.Host,
-		server.Port, server.User,
-		server.Database,
-		logFile))
-
-	fmt.Println(psqlCmd.String())
-	// Create pgbadger command
-	//pgbadgerCmd := exec.Command("perl", "pgbadger", "-f", "stderr", "-v", "-o", reportPath, "-")
-	pgbadgerCmd := getOSCommand(fmt.Sprintf("perl pgbadger -f stderr -v -o %s - ", reportPath))
-
-	fmt.Println(pgbadgerCmd.String())
-
 	// Create output file
 	outputFile, err := os.Create(outputPath)
 	if err != nil {
@@ -95,84 +75,49 @@ func GenerateReport(c *gin.Context, serverName string, reportDir string) {
 		return
 	}
 
-	// Create pipes to connect the commands
-	r, w := io.Pipe()
+	// Create psql command to fetch log content
+	psqlCmd := exec.Command("psql", "-A", "-q", "-t",
+		"-h", server.Host,
+		"-p", fmt.Sprintf("%d", server.Port),
+		"-U", server.User,
+		"-d", server.Database,
+		"-c", fmt.Sprintf("SELECT pg_read_file('pg_log/%s');", logFile))
 
-	// Set up pgbadger command to read from the pipe
-	pgbadgerCmd.Stdin = r
+	fmt.Println(psqlCmd.String())
+	pgbadgerCmd := exec.Command("perl", "pgbadger", "-f", "stderr", "-v", "-o", reportPath, "-")
+
 	pgbadgerCmd.Stdout = outputFile
 	pgbadgerCmd.Stderr = outputFile
 
-	// Start pgbadger
+	psqlCmd.Stderr = outputFile
+
+	pipe, _ := psqlCmd.StdoutPipe()
+	pgbadgerCmd.Stdin = pipe
+
+	fmt.Println(pgbadgerCmd.String())
+
+	var processes []*exec.Cmd
+	processes = append(processes, psqlCmd, pgbadgerCmd)
+	// Store the process
+	reportProcesses.Store(reportPath, &ReportProcess{
+		Cmd:       processes,
+		StartTime: time.Now(),
+	})
+
 	if err := pgbadgerCmd.Start(); err != nil {
-		r.Close()
-		w.Close()
 		outputFile.Close()
 		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to start pgbadger: %v", err)})
 		return
 	}
 
-	// Store the process
-	reportProcesses.Store(reportPath, &ReportProcess{
-		Cmd:       pgbadgerCmd,
-		StartTime: time.Now(),
-	})
-
-	// Run psql and process its output in a goroutine
 	go func() {
-		defer w.Close()
 		defer outputFile.Close()
 		defer reportProcesses.Delete(reportPath)
-
-		// Set up stdout pipe for psql
-		stdout, err := psqlCmd.StdoutPipe()
-		if err != nil {
-			fmt.Fprintf(outputFile, "Error creating stdout pipe: %v\n", err)
-			return
-		}
-
-		// Start the psql command
 		if err := psqlCmd.Start(); err != nil {
-			fmt.Fprintf(outputFile, "Error starting psql command: %v\n", err)
+			outputFile.Close()
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to start psql: %v", err)})
 			return
 		}
-
-		// Process the output line by line
-		scanner := bufio.NewScanner(stdout)
-		var lineCount int
-		var isFirstLine = true
-
-		// Read and process each line
-		for scanner.Scan() {
-			lineCount++
-			text := scanner.Text()
-
-			// Skip the first line
-			if isFirstLine {
-				isFirstLine = false
-				continue
-			}
-
-			// Skip the last line that starts with '(' and ends with ')'
-			if len(text) > 0 && text[0] == '(' && text[len(text)-1] == ')' {
-				continue
-			}
-
-			// Write the line to the pipe
-			fmt.Fprintln(w, text)
-		}
-
-		// Check for scanning errors
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintf(outputFile, "Error reading psql output: %v\n", err)
-		}
-
-		/*// Wait for psql to finish
-		if err := psqlCmd.Wait(); err != nil {
-			fmt.Fprintf(outputFile, "Error waiting for psql command: %v\n", err)
-		}*/
-
-		// Wait for pgbadger to finish
 		if err := pgbadgerCmd.Wait(); err != nil {
 			fmt.Printf("Error generating report: %v\n", err)
 		}
@@ -187,19 +132,23 @@ func GenerateReport(c *gin.Context, serverName string, reportDir string) {
 func StopReport(c *gin.Context, serverName string, reportName string, reportDir string) {
 	reportPath := filepath.Join(reportDir, serverName, reportName)
 
-	processInterface, exists := reportProcesses.Load(reportPath)
+	reportKey := strings.TrimSuffix(reportPath, ".log") + ".html"
+	processInterface, exists := reportProcesses.Load(reportKey)
 	if !exists {
+		printActiveReports()
 		c.JSON(http.StatusNotFound, gin.H{"error": "No active report generation found"})
 		return
 	}
 
-	process := processInterface.(*ReportProcess)
-	if err := process.Cmd.Process.Kill(); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to stop report generation: %v", err)})
-		return
+	repProc := processInterface.(*ReportProcess)
+	for _, proc := range repProc.Cmd {
+		if err := killProcessTree(proc.Process.Pid); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to stop proc %d report generation: %v", proc.Process.Pid, err)})
+			return
+		}
 	}
 
-	reportProcesses.Delete(reportPath)
+	reportProcesses.Delete(reportKey)
 	c.JSON(http.StatusOK, gin.H{"message": "Report generation stopped"})
 }
 
@@ -233,4 +182,61 @@ func GetReportStatus(c *gin.Context, serverName string, reportName string, repor
 		"startTime": process.StartTime,
 		"output":    string(output),
 	})
+}
+
+func printActiveReports() {
+	reportProcesses.Range(func(key, value interface{}) bool {
+		fmt.Printf("Key: %v, Value: %v\n", key, value)
+		return true
+	})
+}
+
+func killProcessTree(pid int) error {
+	if runtime.GOOS == "windows" {
+		// Windows: taskkill /T /F /PID <pid>
+		cmd := exec.Command("taskkill", "/F", "/T", "/PID", strconv.Itoa(pid))
+		cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+		return cmd.Run()
+	} else {
+		// Linux/macOS: рекурсивное завершение процессов
+		return killProcessTreeUnix(pid)
+	}
+}
+
+// killProcessTreeUnix завершает процесс и его дочерние процессы (Linux/macOS)
+func killProcessTreeUnix(pid int) error {
+	// Получаем дочерние процессы
+	childPIDs, err := findChildProcesses(pid)
+	if err != nil {
+		return err
+	}
+
+	// Убиваем дочерние процессы
+	for _, childPID := range childPIDs {
+		_ = killProcessTreeUnix(childPID) // Рекурсия
+	}
+
+	// Убиваем сам процесс
+	process, err := os.FindProcess(pid)
+	if err == nil {
+		return process.Kill()
+	}
+	return err
+}
+
+// findChildProcesses ищет дочерние процессы (Linux/macOS)
+func findChildProcesses(pid int) ([]int, error) {
+	cmd := exec.Command("pgrep", "-P", strconv.Itoa(pid))
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	var childPIDs []int
+	for _, line := range output {
+		childPID, _ := strconv.Atoi(string(line))
+		childPIDs = append(childPIDs, childPID)
+	}
+
+	return childPIDs, nil
 }
